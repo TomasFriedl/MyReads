8503580fa276dd83d971436e56bc1da4baef3c57
Tomáš Friedl <tomas.friedl@eon.cz>
2023-10-23 16:44:08 +0200

    Change

--- src/App.js
+++ src/App.js
@@ -5,15 +5,31 @@ import "./App.css";
 import Categories from "./components/Categories";
 import SearchBooks from "./components/SearchBooks";
 
-const BooksApp = () => {
-	const [fetchedBooks, setFetchedBooks] = useState([]);
+const MyReads = () => {
+
+	const [books, setBooks] = useState([]);
+
+	const updateBookShelf = (updatedBook, newShelf) => {
+		BooksAPI.update(updatedBook, newShelf)
+			.then(() => {
+				if (newShelf === "none") {
+					setBooks((prevBooks) => prevBooks.filter((book) => book.id !== updatedBook.id));
+				} else {
+					updatedBook.shelf = newShelf;
+					setBooks((prevBooks) => [...prevBooks.filter((book) => book.id !== updatedBook.id), updatedBook]);
+				}
+			})
+			.catch((error) => {
+				console.error("Error updating book shelf:", error);
+			});
+	};
 
 	useEffect(() => {
-		const fetchData = async () => {
+		const fetchBooks = async () => {
 			try {
 				const response = await BooksAPI.getAll();
-				if (!response.error) {
-					setFetchedBooks(response);
+				if (response && !response.error) {
+					setBooks(response);
 				} else {
 					console.error("Failed to fetch books:", response.error);
 				}
@@ -21,37 +37,24 @@ const BooksApp = () => {
 				console.error("Error fetching books:", error);
 			}
 		};
-		fetchData();
+		fetchBooks();
 	}, []);
-
-	const changeBookShelf = (book, shelf) => {
-		BooksAPI.update(book, shelf).then(() => {
-			if (shelf === "none") {
-				setFetchedBooks((prevBooks) => prevBooks.filter((b) => b.id !== book.id));
-			} else {
-				book.shelf = shelf;
-				setFetchedBooks((prevBooks) => prevBooks.filter((b) => b.id !== book.id).concat(book));
-			}
-		}).catch((error) => {
-			console.error("Error updating book shelf:", error);
-		});
-	};
 
 	return (
 		<Router>
-			<div className='app'>
+			<div className="app">
 				<Switch>
-					<Route exact path='/search'>
-						<SearchBooks changeBookShelf={changeBookShelf} fetchedBooks={fetchedBooks} />
+					<Route exact path="/search">
+						<SearchBooks changeBookShelf={updateBookShelf} fetchedBooks={books} />
 					</Route>
-					<Route exact path='/'>
-						<div className='list-books'>
-							<div className='list-books-title'>
+					<Route exact path="/">
+						<div className="list-books">
+							<div className="list-books-title">
 								<h1>MyReads</h1>
 							</div>
-							<Categories books={fetchedBooks} onShelfChange={changeBookShelf} />
-							<div className='open-search'>
-								<Link className="open-search a" to='/search'></Link>
+							<Categories books={books} onShelfChange={updateBookShelf} />
+							<div className="open-search">
+								<Link className="open-search a" to="/search"></Link>
 							</div>
 						</div>
 					</Route>
@@ -61,4 +64,4 @@ const BooksApp = () => {
 	);
 };
 
-export default BooksApp;
+export default MyReads;
--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -3,33 +3,38 @@ import * as BooksAPI from "../BooksAPI";
 import GetBooks from "./GetBooks";
 import { Link } from "react-router-dom";
 
-const SearchPage = ({ changeBookShelf, fetchedBooks }) => {
-	const [term, setTerm] = useState("");
-	const [books, setBooks] = useState([]);
+const SearchBooks = ({ changeShelf, books }) => {
+	const [searchTerm, setSearchTerm] = useState("");
+	const [searchResults, setSearchResults] = useState([]);
+
+	const handleSearch = async (term) => {
+		try {
+			const response = await BooksAPI.search(term);
+			if (response && !response.error) {
+				setSearchResults(response);
+			} else {
+				setSearchResults([]);
+			}
+		} catch (error) {
+			console.error("Error searching books: ", error);
+			setSearchResults([]);
+		}
+	};
+
+	const delayedSearch = (term) => {
+		const searchTimer = setTimeout(() => {
+			handleSearch(term);
+		}, 300);
+		return () => clearTimeout(searchTimer);
+	};
 
 	useEffect(() => {
-		const searchBooks = async () => {
-			if (term.length !== 0) {
-				try {
-					const searchedBooks = await BooksAPI.search(term);
-					if (!searchedBooks.error) {
-						setBooks(searchedBooks);
-					} else {
-						setBooks([]);
-					}
-				} catch (error) {
-					console.error("Error searching books: ", error);
-					setBooks([]);
-				}
-			} else {
-				setBooks([]);
-			}
-		};
-		
-		const searchTimer = setTimeout(searchBooks, 300);
-
-		return () => clearTimeout(searchTimer);
-	}, [term]);
+		if (searchTerm.trim() === "") {
+			setSearchResults([]);
+		} else {
+			return delayedSearch(searchTerm);
+		}
+	}, [searchTerm]);
 
 	return (
 		<div className='search-books'>
@@ -38,21 +43,21 @@ const SearchPage = ({ changeBookShelf, fetchedBooks }) => {
 				<div className='search-books-input-wrapper'>
 					<input
 						type='text'
-						value={term}
-						onChange={(e) => setTerm(e.target.value)}
+						value={searchTerm}
+						onChange={(e) => setSearchTerm(e.target.value)}
 						placeholder='Search by title or author'
 					/>
 				</div>
 			</div>
 			<div className='search-books-results'>
 				<GetBooks
-					searchedBooks={books}
-					fetchedBooks={fetchedBooks}
-					changeBookShelf={changeBookShelf}
+					searchedBooks={searchResults}
+					books={books}
+					changeShelf={changeShelf}
 				/>
 			</div>
 		</div>
 	);
 };
 
-export default SearchPage;
+export default SearchBooks;
